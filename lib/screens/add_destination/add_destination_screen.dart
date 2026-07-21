bd90987519da108e3ef544fd1f6c701a98136599
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-21 13:56:11 +0200

    Add AddDestinationScreen widget with basic layout

---
+++ lib/screens/add_destination/add_destination_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddDestinationScreen extends StatelessWidget {
+  const AddDestinationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Add Destinantion Screen"),
+      ),
+    );
+  }
+}
